d7fcbbe005f0f0bad4b2ac9a114d082acf46ac44
Han Lie <133509785+johan99999@users.noreply.github.com>
2024-05-29 17:21:55 +0700

    Update TombolLampu.java

--- TombolLampu.java
+++ TombolLampu.java
@@ -1,17 +1,18 @@
+package com.dicoding.java.controlflow;
 import java.util.Objects;
 import java.util.Scanner;
 
-public class ifThen {
+public class TombolLampu {
     public static void main(String[] args){
-        var run = "e";
-        while (run == "e"){
+        var run = true;
+        while (run){
             System.out.print("tekan tombol lampu? y/n : ");
             Scanner input = new Scanner(System.in);
             var power = input.next();
-            boolean isOn = false;
+            boolean isOn;
             if (Objects.equals(power, "y")){
                 isOn = true;
-                System.out.println("Menyalakan lampu");
+                System.out.println("*Menyalakan lampu*");
                 System.out.println("Lampunya nyala? : " + isOn);
                 System.out.println("   ====    ");
                 System.out.println("   ||||    ");
@@ -22,19 +23,32 @@ public class ifThen {
                 System.out.println(" *      *");
 
 
+
+
+
             } else if (Objects.equals(power, "n")) {
                 isOn = false;
-                System.out.println("Mematikan lampu");
+                System.out.println("*Mematikan lampu*");
                 System.out.println("Lampunya nyala? : " + isOn);
                 System.out.println("   ====    ");
                 System.out.println("   ||||    ");
                 System.out.println("  {    }   ");
                 System.out.println("  {____}   ");
+            } else{
+                System.out.println("Keluar");
+                run = false;
             }
 
         }
 
 
 
+        }
+
+
+
     }
-}
+
+
+
+
